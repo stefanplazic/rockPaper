e8c680812a295ab448041ef00e630a570d7b306a
stefanplazic <stefan.plazi@gmail>
2019-11-28 13:35:38 +0100

    Error handling

--- index.js
+++ index.js
@@ -16,17 +16,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/api/session', sessionController);
 app.use('/api/player', playerController);
 
-app.use(function (req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-
-app.use(function (err, req, res, next) {
-    res.status(err.status || 500);
-    res.send({code:1});
-});
 
 app.listen(port, () => {
     console.log(`Server is up on ${port}`);
